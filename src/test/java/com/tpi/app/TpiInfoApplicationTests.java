6fead5a388cb62c799800a83e47db55a6df16394
b4ngi <ignaciobenetucci@gmail.com>
2022-11-13 19:14:54 -0300

    commit inicial

---
+++ src/test/java/com/tpi/app/TpiInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
